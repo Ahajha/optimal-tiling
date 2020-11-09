a9e76b5df682dec3ebd1e42d2b0ab1d6d28427a0
Alex <ahajha@gmail.com>
2020-11-09 12:40:08 -0500

    various improvements to fraction class

--- src/fraction.cpp
+++ src/fraction.cpp
@@ -2,15 +2,12 @@
 #include <iostream>
 #include <compare>
 
-fraction::fraction() : num(0), den(0) {}
-fraction::fraction(unsigned n, unsigned d) : num(n), den(d) {}
-
 std::strong_ordering fraction::operator<=>(const fraction& other) const
 {
 	// 0 cases
 	if (den == 0) return 0 <=> other.num;
 	if (other.den == 0) return num <=> 0;
-
+	
 	auto result = (num * other.den) <=> (other.num * den);
 	
 	// If they are equal, say the one with the smaller denominator
@@ -20,7 +17,6 @@ std::strong_ordering fraction::operator<=>(const fraction& other) const
 
 std::ostream& operator<<(std::ostream& stream, const fraction& f)
 {
-	stream << f.num << "/" << f.den << " = " << ((double)f.num/f.den);
-	
-	return stream;
+	return stream << f.num << "/" << f.den
+		<< " = " << static_cast<double>(f.num)/f.den;
 }
--- src/fraction.hpp
+++ src/fraction.hpp
@@ -8,14 +8,13 @@
 struct fraction
 {
 	// den == 0 implies num == 0. Not the most intuitive, but makes some other
-	// things here simpler.
+	// things here simpler. In the future, will default to 0/1 instead.
 	unsigned num, den;
 	
 	std::strong_ordering operator<=>(const fraction& other) const;
 	
-	// Defaults to 0, otherwise takes a specified numerator and denominator.
-	fraction();
-	fraction(unsigned, unsigned);
+	// Defaults to 0/0, can specify either just numerator or both.
+	fraction(unsigned n = 0, unsigned d = 0) : num(n), den(d) {}
 };
 
 std::ostream& operator<<(std::ostream&, const fraction&);
