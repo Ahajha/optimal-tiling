2f26850659bfa0737c119635787140c8c9cc059a
Alex <ahajha@gmail.com>
2020-12-24 13:22:25 -0500

    minor tweaks to variadic-array

--- src/variadic-array.hpp
+++ src/variadic-array.hpp
@@ -3,20 +3,25 @@
 
 /*
 The purpose of this template is to convert a list of template
-parameters into a consteval std::array of those parameters.
+parameters into a constexpr std::array of those parameters.
 */
 
 #include <array>
 
+// Base case, for zero-length template lists.
 template<class T, T... ts>
-struct variadic_array : public std::array<T, sizeof...(ts)> {};
+struct variadic_array : public std::array<T, 0> {};
 
+// Recursive case.
 template<class T, T t1, T ... ts>
 struct variadic_array<T,t1,ts...> : public std::array<T, sizeof...(ts) + 1>
 {
 	constexpr variadic_array()
 	{
+		// Store a copy of a one-smaller sized array.
 		constexpr variadic_array<T,ts...> sa;
+		
+		// Copy the smaller array in and add the first element.
 		std::copy(sa.begin(), sa.end(), this->begin() + 1);
 		this->front() = t1;
 	}
