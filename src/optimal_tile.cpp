9a754a619104d5bc49a1dd9694e839c868a5d578
Alex <ahajha@gmail.com>
2021-01-09 01:13:27 -0500

    replace test program with mostly working tile program

--- src/optimal_tile.cpp
+++ src/optimal_tile.cpp
@@ -1,79 +1,158 @@
 #include "slice.hpp"
-#include <iostream>
-#include <iomanip>
+#include "fraction.hpp"
 
 // A macro named DIM_SIZES will be compiled in.
 
-// For the time being, this will serve as a testing file
-
-template<unsigned ... ds>
-void testBase()
+// Structures relating to the enumeration algorithm
+struct path_info
 {
-	std::array<unsigned,sizeof...(ds)> dims {{ds...}};
+	// Number of induced vertices in this path, the vertex
+	// that precedes this one in this path.
+	unsigned num_induced, second_to_last;
+};
+
+// Index [len][end] gives info about the 'len' long path with
+// 'end' as the last vertex.
+typedef std::vector<std::vector<path_info>> path_info_matrix;
+
+template<std::unsigned_integral T, T ... dims>
+struct tile
+{
+	std::vector<unsigned> slices;
+	fraction density;
 	
-	std::cout << "Dimensions\n";
-	for (auto i : dims)
-	{
-		std::cout << i << ' ';
-	}
-	std::cout << '\n';
+	tile() {}
 	
-	std::cout << "Permutations\n";
-	for (const auto& perm : permutationSet<unsigned,ds...>::perms)
+	tile(const path_info_matrix& paths_info, unsigned len,
+		unsigned end, fraction dens) : slices(len), density(dens)
 	{
-		for (auto i : perm)
+		unsigned currentVertex = end;
+		for (unsigned length = len; length > 0; length--)
 		{
-			std::cout << std::setw(2) << i << ' ';
+			slices[length - 1] = currentVertex;
+			
+			currentVertex = paths_info[length][currentVertex].second_to_last;
 		}
-		std::cout << '\n';
 	}
+};
+
+template<std::unsigned_integral T, T ... dims>
+std::ostream& operator<<(std::ostream& stream, const tile<T,dims...>& t)
+{
+	stream << t.density << '\n';
+	for (unsigned vertex : t.slices)
+	{
+		stream << slice_graph<true,T,dims...>::lookup(vertex)
+			<< slice_defs::er_store
+				[slice_graph<true,T,dims...>::graph[vertex].erID]
+			<< "\n\n";
+	}
+	return stream;
 }
 
-template<bool prune, unsigned ... ds>
-void test()
+template<std::unsigned_integral T, T, T ... rest>
+void enumerate(auto start_time)
 {
-	using graph_t = slice_graph<prune,unsigned,ds...>;
+	slice_graph<true,T,rest...>::enumerate();
 	
-	graph_t::enumerate();
+	std::cout << slice_graph<true,T,rest...>::slices.size()
+		<< " slices generated and adjacency lists "
+		<< "filled in " << (float)(clock()-start_time)/(CLOCKS_PER_SEC)
+		<< " seconds" << std::endl;
+}
+
+/*------------------------------------
+Prints the maximum size and density of
+every rectangle sized 1xn through nxn.
+Also, prints the maximum density tile.
+------------------------------------*/
+
+template<std::unsigned_integral T, T ... dims>
+void findMaxTilingWithStart(unsigned start, tile<T,dims...>& bestTile)
+{
+	using slice = slice_graph<true,T,dims...>;
 	
-	std::cout << "Slices\n";
-	for (const auto& s : graph_t::slices)
+	// Information matrix, size is nearly square. The inner vector needs to be
+	// the same size as the slice graph. The outer vector needs to be one larger,
+	// so that when enumerating tiles, all tiles with all slices would technically
+	// be accounted for.
+	
+	// TODO: The outer vector likely can be smaller, experimentally no maximum tiles
+	// have been longer than 10. Perhaps grow slowly, occasionally checking for
+	// duplicate slices.
+	path_info_matrix paths_info(
+		slice::graph.size() + 1, std::vector<path_info>(slice::graph.size(),{0,0})
+	);
+	
+	constexpr unsigned num_verts = (dims * ...);
+	
+	// Only initialize a single cell, since here we have a specific starting vertex.
+	paths_info[1][start].num_induced = slice::lookup(start).numVerts;
+	
+	// For each length
+	for (unsigned len = 2; len < paths_info.size(); ++len)
 	{
-		std::cout << s << '\n';
-	}
-	
-	std::cout << "Graph\n";
-	for (unsigned i = 0; i < graph_t::graph.size(); ++i)
-	{
-		std::cout << std::setw(3) << i << ":";
-		for (unsigned adj : graph_t::graph[i].adjList)
+		// Try to expand each cell that has a valid path
+		for (unsigned end = 0; end < slice::graph.size(); ++end)
 		{
-			std::cout << ' ' << adj;
+			const unsigned oldNV = paths_info[len - 1][end].num_induced;
+			
+			// Expand in every possible way
+			for (unsigned adj : slice::graph[end].adjList)
+			{
+				// Reference, for brevity
+				path_info& info = paths_info[len][adj];
+				
+				// Number of vertices that are added with the new slice
+				const unsigned newNV = oldNV + slice::lookup(adj).numVerts;
+				
+				if (info.num_induced < newNV)
+				{
+					info = {newNV, end};
+					
+					// If a cycle has been found, check to see if it is the new best
+					if (adj == start)
+					{
+						fraction density(oldNV, (len - 1) * num_verts);
+						
+						if (density > bestTile.density)
+						{
+							bestTile = tile<T,dims...>(paths_info, len - 1,
+								end, density);
+							
+							std::cout << "found: " << bestTile;
+						}
+					}
+				}
+			}
 		}
-		std::cout << '\n';
 	}
 }
 
-template<unsigned ... ds>
-void testPruned()
+template<std::unsigned_integral T, T ... dims>
+void findMaxTiling()
 {
-	test<true,ds...>();
-}
-
-template<unsigned ... ds>
-void testUnpruned()
-{
-	test<false,ds...>();
+	tile<T,dims...> bestTile;
+	
+	for (unsigned start = 0; start < slice_graph<true,T,dims...>::slices.size(); ++start)
+	{
+		findMaxTilingWithStart<T,dims...>(start,bestTile);
+	}
 }
 
 int main()
 {
-	std::cout << "Base tests:\n";
-	testBase<DIM_SIZES>();
+	auto start_time = clock();
 	
-	std::cout << "\nUnpruned tests:\n";
-	testUnpruned<DIM_SIZES>();
+	slice_graph<true,unsigned,DIM_SIZES>::enumerate();
 	
-	std::cout << "\nPruned tests:\n";
-	testPruned<DIM_SIZES>();
+	std::cout << slice_graph<true,unsigned,DIM_SIZES>::slices.size()
+		<< " slices generated and adjacency lists "
+		<< "filled in " << (float)(clock()-start_time)/(CLOCKS_PER_SEC)
+		<< " seconds" << std::endl;
+	
+	findMaxTiling<unsigned,DIM_SIZES>();
+	
+	std::cout << "Finished in " << (float)(clock()-start_time)/(CLOCKS_PER_SEC)
+		<< " seconds" << std::endl;
 }
