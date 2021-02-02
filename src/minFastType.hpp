fb853f3fb4699fd469d0195278ff7d198b23ebab
Alex Trotta <ahajha@gmail.com>
2021-02-02 15:23:03 -0500

    use std::conditional_t alias

--- src/minFastType.hpp
+++ src/minFastType.hpp
@@ -13,11 +13,10 @@ as well as the value cast to that type.
 template<uintmax_t maxVal>
 struct minFastType
 {
-	using type = std::conditional<maxVal <= UINT8_MAX,  uint_fast8_t,
-	    typename std::conditional<maxVal <= UINT16_MAX, uint_fast16_t,
-	    typename std::conditional<maxVal <= UINT32_MAX, uint_fast32_t,
-	                                                    uint_fast64_t
-	    >::type>::type>::type;
+	using type = std::conditional_t<maxVal <= UINT8_MAX,  uint_fast8_t,
+	             std::conditional_t<maxVal <= UINT16_MAX, uint_fast16_t,
+	             std::conditional_t<maxVal <= UINT32_MAX, uint_fast32_t,
+	                                                      uint_fast64_t>>>;
 	
 	constexpr static type value = static_cast<type>(maxVal);
 };
