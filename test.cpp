eb63a50cc220f549258c17b8a8364abdea339b4e
Alex Trotta <ahajha@gmail.com>
2022-02-23 05:31:03 -0500

    add framework for more in-depth static_hrp_graph testsC

--- test.cpp
+++ test.cpp
@@ -1,21 +1,63 @@
 #include "static_graph.hpp"
-#include <concepts>
+#include <array>
+#include <vector>
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest/doctest.h"
+
+template<class container_t, class graph_t>
+void compare_neighbors(const container_t& neighbors, const graph_t& graph)
+{
+	for (typename graph_t::vertex_id i = 0; i < graph.n_vertices; ++i)
+	{
+		CHECK(neighbors[i].size() == graph.vertices[i].neighbors.size());
+		
+		for (std::size_t j = 0; j < neighbors[i].size(); ++j)
+		{
+			CHECK(neighbors[i][j] == graph.vertices[i].neighbors[j]);
+		}
+	}
+}
+
+template<class container_t, class graph_t>
+void compare_directions(const container_t& directions, const graph_t& graph)
+{
+	for (typename graph_t::vertex_id i = 0; i < graph.n_vertices; ++i)
+	{
+		CHECK(directions[i].size() == graph.vertices[i].directions.size());
+		
+		for (std::size_t j = 0; j < directions[i].size(); ++j)
+		{
+			CHECK(directions[i][j] == graph.vertices[i].directions[j]);
+		}
+	}
+}
 
 TEST_CASE("dimensions: {1}")
 {
 	static_hrp_graph<1> graph;
 	
 	CHECK(graph.n_vertices == 1);
-	
-	CHECK(graph.vertices[0].neighbors.size() == 0);
-	
-	CHECK(graph.vertices[0].directions[0] == graph.no_vertex);
-	CHECK(graph.vertices[0].directions[1] == graph.no_vertex);
-	
 	CHECK(std::same_as<decltype(graph)::vertex_id, std::uint8_t>);
+	
+	std::array<std::vector<decltype(graph)::vertex_id>, graph.n_vertices> neighbors
+	{
+		{
+			{}
+		}
+	};
+	
+	compare_neighbors(neighbors, graph);
+	
+	std::array<std::array<decltype(graph)::vertex_id, 2>, graph.n_vertices> directions
+	{
+		{
+			graph.no_vertex,
+			graph.no_vertex
+		}
+	};
+	
+	compare_directions(directions, graph);
 }
 
 TEST_CASE("dimensions: {1,1}")
