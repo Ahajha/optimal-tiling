30237162c53b7ebb504d7d4f7092061492bb654d
Alex Trotta <ahajha@gmail.com>
2022-02-23 03:30:16 -0500

    resolve all remaining compile errors

--- include/static_graph.hpp
+++ include/static_graph.hpp
@@ -25,6 +25,52 @@ class static_hrp_graph
 	
 	constexpr static vertex_id no_vertex = std::numeric_limits<vertex_id>::max();
 	
+	std::array<vertex_id, n_vertices> vertices;
+	
+	// Returns the number of vertices that would be in the graph if
+	// it were truncated to a given number of dimensions Note that
+	// if d == dim_array.size(), this is just the number of vertices.
+	[[nodiscard]] constexpr static std::size_t size_of_dim(std::size_t d)
+	{
+		constexpr auto size_table = []
+		{
+			std::array<std::size_t, dims_array.size()> table;
+			
+			table.front() = 1;
+			for (std::size_t i = 1; i < table.size(); ++i)
+			{
+				table[i] = table[i - 1] * dims_array[i];
+			}
+			
+			return table;
+		}();
+		
+		return size_table[d];
+	}
+	
+	// Gets a specific dimension of the coordinate of c. Valid values of
+	// d are 0 <= d < dim_array.size()
+	[[nodiscard]] constexpr static vertex_id get_coord (std::size_t d, vertex_id vid)
+	{
+		return (vid / size_of_dim(d)) % dims_array[d];
+	}
+	
+	// Returns the vertex id of the vertex "forward" in a
+	// dimension d from c, if it exists, EMPTY if not.
+	[[nodiscard]] constexpr static vertex_id forward   (std::size_t d, vertex_id vid)
+	{
+		return (get_coord(d,vid) == dims_array[d] - 1)
+			? no_vertex : vid + size_of_dim(d);
+	}
+
+	// Returns the vertex id of the vertex "backward" in a
+	// dimension d from c, if it exists, EMPTY if not.
+	[[nodiscard]] constexpr static vertex_id backward  (std::size_t d, vertex_id vid)
+	{
+		return (get_coord(d,vid) == 0)
+			? no_vertex : vid - size_of_dim(d);
+	}
+	
 	struct vertex
 	{
 		// Neighbors are the vertices it actually
@@ -58,66 +104,17 @@ class static_hrp_graph
 		}
 	};
 	
-	// Index of a given vertex is its ID
-	constexpr static auto vertices = []
+	[[nodiscard]] constexpr static_hrp_graph()
 	{
-		std::array<vertex, n_vertices> vertices_;
-		
-		for (vertex_id i = 0; i < vertices_.size(); ++i)
+		for (vertex_id i = 0; i < vertices.size(); ++i)
 		{
-			vertices_[i] = vertex(i);
+			vertices[i] = vertex(i);
 		}
-		
-		return vertices_;
-	}();
+	} 
 	
 	// Returns true iff vid is an element on the outer shell of the hypercube.
-	[[nodiscard]] static constexpr bool is_on_outer_shell(vertex_id vid)
+	[[nodiscard]] constexpr bool is_on_outer_shell(vertex_id vid) const
 	{
 		return vertices[vid].neighbors.size() != dims_array.size() * 2;
-	}
-	
-	// Returns the number of vertices that would be in the graph if
-	// it were truncated to a given number of dimensions Note that
-	// if d == dim_array.size(), this is just the number of vertices.
-	[[nodiscard]] static constexpr std::size_t size_of_dim(std::size_t d)
-	{
-		constexpr auto size_table = []
-		{
-			std::array<std::size_t, dims_array.size()> table;
-			
-			table.front() = 1;
-			for (std::size_t i = 1; i < table.size(); ++i)
-			{
-				table[i] = table[i - 1] * dims_array[i];
-			}
-			
-			return table;
-		}();
-		
-		return size_table[d];
-	}
-	
-	// Gets a specific dimension of the coordinate of c. Valid values of
-	// d are 0 <= d < dim_array.size()
-	[[nodiscard]] static constexpr vertex_id get_coord (std::size_t d, vertex_id vid)
-	{
-		return (vid / size_of_dim(d)) % dims_array[d];
-	}
-	
-	// Returns the vertex id of the vertex "forward" in a
-	// dimension d from c, if it exists, EMPTY if not.
-	[[nodiscard]] static constexpr vertex_id forward   (std::size_t d, vertex_id vid)
-	{
-		return (get_coord(d,vid) == dims_array[d] - 1)
-			? no_vertex : vid + size_of_dim(d);
-	}
-
-	// Returns the vertex id of the vertex "backward" in a
-	// dimension d from c, if it exists, EMPTY if not.
-	[[nodiscard]] static constexpr vertex_id backward  (std::size_t d, vertex_id vid)
-	{
-		return (get_coord(d,vid) == 0)
-			? no_vertex : vid - size_of_dim(d);
 	}
 };
