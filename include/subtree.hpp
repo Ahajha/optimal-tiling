0fcb538d61010c6575938adcd0d4a5787b683d09
Alex Trotta <ahajha@gmail.com>
2022-03-13 01:25:46 -0500

    fix subtree implementation based on tests

--- include/subtree.hpp
+++ include/subtree.hpp
@@ -12,7 +12,11 @@
 template<class graph_t>
 struct subtree_vertex
 {
+	// Induced is whether the vertex is induced in the current subtree.
 	bool induced{false};
+	
+	// Effective degree is the number of neighbors that are induced. Note
+	// that this vertex being induced has no effect on this number.
 	graph_t::vertex_id effective_degree{0};
 };
 
@@ -39,7 +43,7 @@ struct vertices_base<static_hrp_graph<dims...>>
 template<class graph_t>
 class subtree : vertices_base<graph_t>
 {
-	graph_t::vertex_id n_induced;
+	graph_t::vertex_id n_induced_;
 	
 	graph_t::vertex_id root;
 	
@@ -49,42 +53,51 @@ class subtree : vertices_base<graph_t>
 	
 	subtree(graph_t base, graph_t::vertex_id root_id)
 		: vertices_base<graph_t>(base.vertices.size())
-		, n_induced{0}, root{root_id}, base_graph{std::move(base)}
+		, n_induced_{1}, root{root_id}, base_graph{std::move(base)}
 	{
 		vertices_base<graph_t>::vertices[root].induced = true;
+		
+		for (const auto neighbor : base_graph.vertices[root].neighbors)
+			++vertices_base<graph_t>::vertices[neighbor].effective_degree;
 	}
 	
+	// Returns the number of currently induced neighbors of vertex i.
 	graph_t::vertex_id cnt (graph_t::vertex_id i) const
 		{ return vertices_base<graph_t>::vertices[i].effective_degree; }
 	
+	// Returns true iff vertex i is currently induced
 	bool has (graph_t::vertex_id i) const
 		{ return vertices_base<graph_t>::vertices[i].induced; }
 	
+	// Returns true iff i is not the empty vertex ID and this graph contains i.
 	bool exists(graph_t::vertex_id i) const
 		{ return i != graph_t::no_vertex && has(i); }
+	
+	// Returns the number of induced vertices.
+	graph_t::vertex_id n_induced() const
+		{ return n_induced_; }
 	
 	// Assumes i is on the border of the current graph.
 	// Returns true iff the vertex was added.
 	// Currently, does not check for enclosed space.
 	bool add(graph_t::vertex_id i)
 	{
-		vertices_base<graph_t>::vertices[i].induced = true;
-		
 		// This should have exactly one neighbor, we need to validate it
-		auto neighbor = *std::ranges::find_if(base_graph.vertices[i].neighbors, has);
+		const auto sole_neighbor = sole_neighbor_of(i);
 		
-		++vertices_base<graph_t>::vertices[neighbor].effective_degree;
-		
-		if (validate(neighbor))
+		if (validate(sole_neighbor))
 		{
-			++n_induced;
-			vertices_base<graph_t>::vertices[i].effective_degree = 1;
+			vertices_base<graph_t>::vertices[i].induced = true;
+			++n_induced_;
+			
+			for (const auto neighbor : base_graph.vertices[i].neighbors)
+				++vertices_base<graph_t>::vertices[neighbor].effective_degree;
+			
 			return true;
 		}
 		else
 		{
 			// Undo changes made and report that this is invalid
-			--vertices_base<graph_t>::vertices[neighbor].effective_degree;
 			vertices_base<graph_t>::vertices[i].induced = false;
 			return false;
 		}
@@ -94,26 +107,47 @@ class subtree : vertices_base<graph_t>
 	// paired with add().
 	void rem(graph_t::vertex_id i)
 	{
-		--n_induced;
-		
 		vertices_base<graph_t>::vertices[i].induced = false;
+		--n_induced_;
 		
-		--vertices_base<graph_t>::vertices[i].effective_degree;
-		--std::ranges::find_if(base_graph.vertices[i].neighbors, has)->effective_degree;
+		for (const auto neighbor : base_graph.vertices[i].neighbors)
+			--vertices_base<graph_t>::vertices[neighbor].effective_degree;
 	}
 	
+	private:
 	// Defined only for dimension 3. A vertex is valid
 	// if it has at most one axis with both neighbors.
 	bool validate(graph_t::vertex_id i) const
 	{
 		if (base_graph.dims_array.size() == 3)
 		{
-			if (cnt(i) != 4) return cnt(i) < 4;
+			// !!!! This should be called when *considering* this vertex
+			// to be induced, so this condition is really
+			// if (cnt(i) != 4) return cnt(i) < 4;
+			if (cnt(i) != 3) return cnt(i) < 3;
+			
+			auto& dirs = base_graph.vertices[i].directions;
+			
+			// Ensure all axis have at least one neighbor
+			for (auto d = 0ul; d < 3ul; ++d)
+			{
+				// 5 - d gets the opposite direction
+				if (!exists(dirs[d]) && !exists(dirs[5ul - d]))
+					return false;
+			}
+			return true;
 		}
 		else
 		{
 			return true;
 		}
+	}
+	
+	// Assuming i has one neighbor, returns the ID of that neighbor.
+	graph_t::vertex_id sole_neighbor_of(graph_t::vertex_id i)
+	{
+		return *std::ranges::find_if(base_graph.vertices[i].neighbors,
+			[this](auto vid){ return has(vid); });
 	}
 	
 	// Returns true iff there is at least one block whose
