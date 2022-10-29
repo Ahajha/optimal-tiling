e74cbcde0f1194417cf089c36a019c40c60d19b1
Ahajha <ahajha@gmail.com>
2022-10-29 04:03:34 -0400

    Add start of new implementation of induded subtree enumeration

---
+++ source/induced_subtree_enumerate.cpp
@@ -0,0 +1,122 @@
+#include "graph.hpp"
+#include "subtree.hpp"
+
+#include <iostream>
+#include <list>
+#include <stack>
+
+using graph_type = hrp_graph;
+using vertex_id = graph_type::vertex_id;
+using subtree_type = subtree<graph_type>;
+using border_type = std::list<vertex_id>;
+
+enum class action_type { add, rem, stop };
+struct action {
+  action_type type;
+  vertex_id id;
+};
+
+using history_type = std::stack<action>;
+
+std::ostream &operator<<(std::ostream &stream, const subtree_type &sub) {
+  const auto &dims = sub.base().dims_array;
+  if (dims.size() == 2) {
+    const vertex_id d1 = dims[0];
+    const vertex_id d2 = dims[1];
+
+    vertex_id index = 0;
+    for (vertex_id i = 0; i < d1; ++i) {
+      for (vertex_id j = 0; j < d2; ++j) {
+        stream << (sub.has(index) ? 'X' : '_');
+        ++index;
+      }
+      stream << '\n';
+    }
+  } else {
+    // TODO
+  }
+  return stream;
+}
+
+void update(subtree_type &sub, border_type &border, vertex_id id,
+            history_type &history) {
+  /*
+  for each neighborhood node y of x do // increasing ordering of y's ID
+    if cnt(S, y) > 1 then
+      Remove y from B(S);
+      H.push(del, y);
+    else if y > root(S) and y is not in S then
+      Add y to B(S) on the head;
+      H.push(add, y);
+    end if
+  end for
+  */
+
+  /*
+  for (const auto neighbor : sub.base().vertices[id].neighbors) {
+    if (sub.cnt(neighbor) > 1) {
+        // Removing from an arbitrary point in the list is difficult...
+    }
+  }
+  */
+}
+
+void restore(border_type &border, history_type &history) {
+  /*
+  while true do
+    (op, x) <- H.top();
+    H.pop();
+    if op == pivot then
+      break;
+    else
+      B(S).op(x);
+    end if
+  end while
+  */
+}
+
+void modified_rec(subtree_type &sub, border_type &border,
+                  history_type &history) {
+  /*
+  Output S;
+  while B(S) is not empty do
+    x <- the head of B(S);
+    Remove the head of B(S);
+    S <- Ch(S, x);
+    H.push(pivot, NIL);
+    UPDATE(S, B(S), x, H);
+    MODIFIEDREC(S, B(S), H);
+    RESTORE(B(S), H);
+  end while
+  */
+
+  std::cout << sub << '\n';
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
+  /*
+  Number the nodes of G from 1 to |V| as ID;
+  Output {};
+  for each node x in G do
+    S <- {x};
+    B(S) <- an empty doubly linked list;
+    H <- an empty stack;
+    UPDATE(S, B(S), x, H);
+    MODIFIEDREC(S, B(S), H);
+  end for
+  */
+
+  graph_type graph{5, 5};
+
+  // output the empty set - ignoring this one
+
+  for (vertex_id i = 0; i < graph.vertices.size(); ++i) {
+    subtree sub{graph, i};
+
+    border_type border;
+    history_type history;
+
+    update(sub, border, i, history);
+    modified_rec(sub, border, history);
+  }
+}
