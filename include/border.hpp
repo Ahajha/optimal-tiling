2a3726d1e1b39f9b1a7c83f4d8348318fa47b26d
Ahajha <ahajha@gmail.com>
2022-10-30 15:33:17 -0400

    Split border manipulation into separate files

---
+++ include/border.hpp
@@ -0,0 +1,6 @@
+#include "config.hpp"
+
+void update(subtree_type &sub, border_type &border, vertex_id id,
+            history_type &history);
+
+void restore(border_type &border, history_type &history);
--- source/induced_subtree_enumerate.cpp
+++ source/induced_subtree_enumerate.cpp
@@ -1,3 +1,4 @@
+#include "border.hpp"
 #include "config.hpp"
 
 #include <iostream>
@@ -28,65 +29,6 @@ std::ostream &operator<<(std::ostream &stream, const subtree_type &sub) {
     // TODO
   }
   return stream;
-}
-
-void update(subtree_type &sub, border_type &border, vertex_id id,
-            history_type &history) {
-  /*
-  for each neighborhood node y of x do // increasing ordering of y's ID
-    if cnt(S, y) > 1 then
-      Remove y from B(S);
-      H.push(del, y);
-    else if y > root(S) and y is not in S then
-      Add y to B(S) on the head;
-      H.push(add, y);
-    end if
-  end for
-  */
-
-  for (const auto neighbor : sub.base().vertices[id].neighbors) {
-    if (sub.cnt(neighbor) > 1) {
-      if (border.remove(neighbor)) {
-        history.emplace(action_type::rem, neighbor);
-      }
-    } else if (neighbor > sub.root() && !sub.has(neighbor)) {
-      border.push_front(neighbor);
-      history.emplace(action_type::add, neighbor);
-    }
-  }
-}
-
-void restore(border_type &border, history_type &history) {
-  /*
-  while true do
-    (op, x) <- H.top();
-    H.pop();
-    if op == pivot then
-      break;
-    else
-      B(S).op(x);
-    end if
-  end while
-  */
-
-  while (true) {
-    const auto [op, id] = history.top();
-    history.pop();
-
-    switch (op) {
-    case action_type::add: {
-      border.push_front(id);
-      break;
-    }
-    case action_type::rem: {
-      border.remove(id);
-      break;
-    }
-    case action_type::stop: {
-      return;
-    }
-    }
-  }
 }
 
 void modified_rec(subtree_type &sub, border_type &border,
