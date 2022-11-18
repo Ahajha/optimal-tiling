069d5f6e0bc19765bcac56236edaad0cb3e0edda
Ahajha <ahajha@gmail.com>
2022-11-18 00:54:46 -0500

    Add starting tests for permutations

--- test/test_permutation.cpp
+++ test/test_permutation.cpp
@@ -3,8 +3,25 @@
 #include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("Permutations") {
-  std::vector<std::size_t> dims{};
-  const auto perm_set = permutation_set(dims);
-  REQUIRE(perm_set.perms().size() == 1);
-  // CHECK(perm_set.perms().front() == std::vector<std::size_t>{0});
+  SECTION("Dims = {}") {
+    std::vector<std::size_t> dims{};
+    const auto perm_set = permutation_set(dims);
+    REQUIRE(perm_set.perms().size() == 1);
+    CHECK(perm_set.perms() == std::vector<std::vector<vertex_id>>{{0}});
+  }
+
+  SECTION("Dims = {1}") {
+    std::vector<std::size_t> dims{1};
+    const auto perm_set = permutation_set(dims);
+    REQUIRE(perm_set.perms().size() == 1);
+    CHECK(perm_set.perms() == std::vector<std::vector<vertex_id>>{{0}});
+  }
+
+  SECTION("Dims = {2}") {
+    std::vector<std::size_t> dims{2};
+    const auto perm_set = permutation_set(dims);
+    REQUIRE(perm_set.perms().size() == 2);
+    CHECK(perm_set.perms() ==
+          std::vector<std::vector<vertex_id>>{{0, 1}, {1, 0}});
+  }
 }
