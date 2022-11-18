13dce08823f7b0de589b36a0e40e0270f55d23ce
Ahajha <ahajha@gmail.com>
2022-11-18 18:34:05 -0500

    Add 2 x 3 permutation test

--- test/test_permutation.cpp
+++ test/test_permutation.cpp
@@ -44,14 +44,32 @@ TEST_CASE("Permutations") {
     REQUIRE(perm_set.size() == 8);
 
     CHECK(perm_set == std::set<std::vector<vertex_id>>{
-                          {0, 1, 2, 3}, // //
+                          {0, 1, 2, 3},
                           {0, 2, 1, 3},
-                          {1, 0, 3, 2}, // //
+                          {1, 0, 3, 2},
                           {2, 0, 3, 1},
                           {1, 3, 0, 2},
-                          {2, 3, 0, 1}, // //
+                          {2, 3, 0, 1},
                           {3, 1, 2, 0},
-                          {3, 2, 1, 0}, //
+                          {3, 2, 1, 0},
+                      });
+  }
+
+  SECTION("Dims = {3, 2}") {
+    std::vector<std::size_t> dims{3, 2};
+    REQUIRE(permutation_set::n_permutations(dims) == 4);
+
+    const auto perm_vec = permutation_set(dims);
+    REQUIRE(perm_vec.perms().size() == 4);
+
+    const std::set perm_set(perm_vec.perms().begin(), perm_vec.perms().end());
+    REQUIRE(perm_set.size() == 4);
+
+    CHECK(perm_set == std::set<std::vector<vertex_id>>{
+                          {0, 1, 2, 3, 4, 5},
+                          {3, 4, 5, 0, 1, 2},
+                          {2, 1, 0, 5, 4, 3},
+                          {5, 4, 3, 2, 1, 0},
                       });
   }
 }
