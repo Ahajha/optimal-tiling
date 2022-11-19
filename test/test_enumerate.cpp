5ab55cedfc67c1d1e8b201c9c97797bea39ad967
Ahajha <ahajha@gmail.com>
2022-11-19 11:29:37 -0500

    Minor formatting

--- test/test_enumerate.cpp
+++ test/test_enumerate.cpp
@@ -10,16 +10,8 @@ TEST_CASE("Size = {1}") {
   const graph_type graph{1};
 
   const subtree_snapshot_set expected_subtrees{
-      {.cells =
-           {
-               {.count = 0, .has = false},
-           },
-       .n_induced = 0},
-      {.cells =
-           {
-               {.count = 0, .has = true},
-           },
-       .n_induced = 1},
+      {.cells = {{.count = 0, .has = false}}, .n_induced = 0},
+      {.cells = {{.count = 0, .has = true}}, .n_induced = 1},
   };
 
   check_result(graph, expected_subtrees, enumerate);
@@ -29,7 +21,11 @@ TEST_CASE("Size = {2}") {
   const graph_type graph{2};
 
   const subtree_snapshot_set expected_subtrees{
-      {.cells = {{.count = 0, .has = false}, {.count = 0, .has = false}},
+      {.cells =
+           {
+               {.count = 0, .has = false},
+               {.count = 0, .has = false},
+           },
        .n_induced = 0},
       {.cells =
            {
